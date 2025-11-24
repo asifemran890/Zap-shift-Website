aef5b8cddcd982d4b7347d02f9e4b18ed8bcb22e
asifemran890 <freelancerasif890@gmail.com>
2025-11-24 18:16:46 +0600

    added

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -4,6 +4,9 @@ import Home from "../pages/Home/Home/Home";
 import Coverage from "../pages/Coverage/Coverage";
 import About from "../pages/About me/About";
 import Error404 from "../pages/Error404/Error404";
+import AuthLaout from "../Layout/AuthLaout";
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
 
 export const router = createBrowserRouter([
   {
@@ -26,6 +29,20 @@ export const router = createBrowserRouter([
       {
         path: "*",
         Component: Error404,
+      },
+    ],
+  },
+  {
+    path: "/",
+    Component: AuthLaout,
+    children: [
+      {
+        path: "login",
+        Component: Login,
+      },
+      {
+        path: "register",
+        Component: Register,
       },
     ],
   },
